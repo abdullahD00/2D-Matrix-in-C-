30f2a139eeb13f68bc8b8274769e719c1a7ad8d9
Abdullah DÜNDAR <64316056+abdullahD00@users.noreply.github.com>
2021-05-14 01:37:47 +0300

    2D_matris

---
+++ 2BoyutluMatris_C.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+
+
+int main() {
+
+int matrix[2][3];
+matrix[0][0]=7;
+matrix[0][1]=3;
+matrix[0][2]=2;
+matrix[1][0]=4;
+matrix[1][1]=1;
+matrix[1][2]=8;
+printf("%d	%d	%d\n",matrix[0][0],matrix[0][1],matrix[0][2]);
+printf("%d	%d	%d\n",matrix[1][0],matrix[1][1],matrix[1][2]);
+
+
+	return 0;
+}
+
